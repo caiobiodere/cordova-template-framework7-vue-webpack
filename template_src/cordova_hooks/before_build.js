de8c3bd50fca1fc2cebaa8e2cf563a1ca37882bf
Oğuz Özcan <centrual@outlook.com>
2017-01-13 14:00:56 +0300

    Github markdown, center image and h1 title.

--- template_src/cordova_hooks/before_build.js
+++ template_src/cordova_hooks/before_build.js
@@ -3,4 +3,18 @@ This script runs before this command:
  cordova build
 */
 
+module.exports = function(context) {
+	const Q = context.requireCordovaModule('q')
+	let deferral = new Q.defer();
+	
+	console.log("Before Build Started!")
+	
+	setTimeout(function(){
+		console.log('Before Build Ended!');
+		deferral.resolve();
+	}, 3000);
+	
+	return deferral.promise;
+}
+
 // index.html'de cordova.js yoksa, otomatik ekle
--- template_src/cordova_hooks/before_deploy.js
+++ template_src/cordova_hooks/before_deploy.js
@@ -4,4 +4,18 @@ This script runs before this commands:
 	cordova run
 */
 
+module.exports = function(context) {
+	const Q = context.requireCordovaModule('q')
+	let deferral = new Q.defer();
+	
+	console.log("Before Deploy Started!")
+	
+	setTimeout(function(){
+		console.log('Before Deploy Ended!');
+		deferral.resolve();
+	}, 3000);
+	
+	return deferral.promise;
+}
+
 // index.html'de cordova.js yoksa, otomatik ekle
