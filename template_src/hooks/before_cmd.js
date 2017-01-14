ae9af79d9e1a40a6ce5f266013c175aa7de909fb
Oğuz Özcan <centrual@outlook.com>
2017-01-14 03:44:02 +0300

    Some tests...

--- template_src/hooks/before_cmd.js
+++ template_src/hooks/before_cmd.js
@@ -1,6 +1,27 @@
-module.exports = function(context) {
+module.exports = function (context) {
 	const Q = context.requireCordovaModule('q'),
-				cheerio = context.requireCordovaModule('cheerio')
+		cheerio = context.requireCordovaModule('cheerio')
+	
+	
+	cont = {
+		"hook": "before_prepare",
+		"opts": {
+			"platforms": ["android", "browser"],
+			"options": {"live-reload": true, "nobuild": true, "argv": []},
+			"verbose": false,
+			"silent": false,
+			"browserify": false,
+			"fetch": false,
+			"nohooks": [],
+			"projectRoot": "C:\\xampp\\htdocs\\cordova_test\\test",
+			"cordova": {"platforms": ["android", "browser"], "plugins": ["cordova-plugin-whitelist"], "version": "6.4.0"},
+			"save": false
+		},
+		"cmdLine": "C:\\Program Files\\nodejs\\node.exe C:\\Program Files\\nodejs\\node_modules\\cordova\\bin\\cordova run --live-reload --nobuild",
+		"cordova": {"binname": "cordova", "raw": {}},
+		"scriptLocation": "C:\\xampp\\htdocs\\cordova_test\\test\\hooks\\before_cmd.js"
+	}
+	
 	
 	let deferral = new Q.defer();
 	
