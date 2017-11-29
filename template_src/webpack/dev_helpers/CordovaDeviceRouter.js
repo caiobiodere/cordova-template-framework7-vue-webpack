5243bb503ac9b46fdd6a35f8756c762441ba035e
Oğuz Özcan <centrual@outlook.com>
2017-11-29 18:29:35 +0300

    Template working again.

--- template_src/webpack/dev_helpers/CordovaDeviceRouter.js
+++ template_src/webpack/dev_helpers/CordovaDeviceRouter.js
@@ -1,25 +1,23 @@
-const deviceJs = require('current-device').default
+const deviceJs = require('current-device').default;
 
 function bodyPrepend(platform) {
-	let body = document.querySelector("body"),
-		script = document.createElement("script")
-	
-	script.onload = function () {
-		console.log("Cordova script loaded!")
-	}
-	
-	script.src = `platform_cordova_files/${platform}/cordova.js`
-	body.appendChild(script)
-	
-	
-	console.log(`${platform} platform cordova.js injected.`)
+  let body = document.querySelector("body"),
+    script = document.createElement("script");
+
+  script.onload = function () {
+    console.log("Cordova script loaded!");
+  };
+
+  script.src = `platform_cordova_files/${platform}/cordova.js`;
+  body.appendChild(script);
+
+
+  console.log(`${platform} platform cordova.js injected.`);
 }
 
-if (deviceJs.desktop())
-	bodyPrepend("browser")
-else if (deviceJs.android())
-	bodyPrepend("android")
+if (deviceJs.android())
+  bodyPrepend("android");
 else if (deviceJs.ios())
-	bodyPrepend("ios")
+  bodyPrepend("ios");
 else
-	alert("Platform not found!")
+  bodyPrepend("browser");
--- template_src/webpack/plugins/CordovaHtmlOutputPlugin.js
+++ template_src/webpack/plugins/CordovaHtmlOutputPlugin.js
@@ -1,4 +1,4 @@
-const cheerio = require("cheerio")
+const cheerio = require("cheerio");
 
 function CordovaHtmlOutputPlugin() {}
 
@@ -6,21 +6,21 @@ CordovaHtmlOutputPlugin.prototype.apply = function (compiler) {
 	compiler.plugin('compilation', (compilation) => {
 		compilation.plugin('html-webpack-plugin-before-html-processing', function (htmlPluginData, callback) {
 			let $ = cheerio.load(htmlPluginData.html),
-				cordovaJsFound = false
-			
+				cordovaJsFound = false;
+
 			$("script").each((index, element) => {
-				if ($(element).attr("src") == "cordova.js")
-					cordovaJsFound = true
-			})
-			
+				if ($(element).attr("src") === "cordova.js")
+					cordovaJsFound = true;
+			});
+
 			if (!cordovaJsFound)
-				$("body").prepend('<script src="cordova.js"></script>')
-			
-			htmlPluginData.html = $.html()
-			callback(null, htmlPluginData)
-			
+				$("body").prepend('<script src="cordova.js"></script>');
+
+			htmlPluginData.html = $.html();
+			callback(null, htmlPluginData);
+
 		})
 	})
-}
+};
 
-module.exports = CordovaHtmlOutputPlugin
+module.exports = CordovaHtmlOutputPlugin;
