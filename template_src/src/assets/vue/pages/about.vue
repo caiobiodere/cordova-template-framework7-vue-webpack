033616ca3b1ab753396b6dedd6129dea658eb1c8
Caio Biodere <caiobiodere@gmail.com>
2019-06-10 09:56:08 +1200

    2.4.1 Fix to use bundle instead of separately components

--- template_src/src/assets/vue/pages/about.vue
+++ template_src/src/assets/vue/pages/about.vue
@@ -8,17 +8,54 @@
       <p>Framework7 is not compatible with all platforms. It is focused only on iOS and Android (Material) to bring the best experience and simplicity.</p>
       <p>Framework7 is definitely for you if you decide to build iOS and Android hybrid app (PhoneGap) or web app that looks like and feels as great native iOS or Android (Material) apps.</p>
     </f7-block>
+    <f7-list>
+      <f7-list-item
+        swipeout
+        title="Facebook"
+        after="17:14"
+        subtitle="New messages from John Doe"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis tellus ut turpis condimentum, ut dignissim lacus tincidunt. Cras dolor metus, ultrices condimentum sodales sit amet, pharetra sodales eros. Phasellus vel felis tellus. Mauris rutrum ligula nec dapibus feugiat. In vel dui laoreet, commodo augue id, pulvinar lacus."
+      >
+        <f7-swipeout-actions left>
+          <f7-swipeout-button overswipe color="green" @click="$f7router.back()">Reply</f7-swipeout-button>
+          <f7-swipeout-button color="blue" @click="$f7router.back()">Forward</f7-swipeout-button>
+        </f7-swipeout-actions>
+        <f7-swipeout-actions right>
+          <f7-swipeout-button @click="$f7router.back()">More</f7-swipeout-button>
+          <f7-swipeout-button color="orange" @click="$f7router.back()">Mark</f7-swipeout-button>
+          <f7-swipeout-button
+            delete
+            overswipe
+            confirm-text="Are you sure you want to delete this item?"
+          >Delete</f7-swipeout-button>
+        </f7-swipeout-actions>
+      </f7-list-item>
+      <f7-list-item
+        swipeout
+        title="Twitter"
+        after="20:00"
+        subtitle="New messages from John Doe"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis tellus ut turpis condimentum, ut dignissim lacus tincidunt. Cras dolor metus, ultrices condimentum sodales sit amet, pharetra sodales eros. Phasellus vel felis tellus. Mauris rutrum ligula nec dapibus feugiat. In vel dui laoreet, commodo augue id, pulvinar lacus."
+      >
+        <f7-swipeout-actions left>
+          <f7-swipeout-button overswipe color="green" @click="$f7router.back()">Reply</f7-swipeout-button>
+          <f7-swipeout-button color="blue" @click="$f7router.back()">Forward</f7-swipeout-button>
+        </f7-swipeout-actions>
+        <f7-swipeout-actions right>
+          <f7-swipeout-button @click="$f7router.back()">More</f7-swipeout-button>
+          <f7-swipeout-button color="orange" @click="$f7router.back()">Mark</f7-swipeout-button>
+          <f7-swipeout-button
+            delete
+            overswipe
+            confirm-text="Are you sure you want to delete this item?"
+          >Delete</f7-swipeout-button>
+        </f7-swipeout-actions>
+      </f7-list-item>
+    </f7-list>
   </f7-page>
 </template>
 <script>
-  import { f7Page, f7Navbar, f7BlockTitle, f7Block } from 'framework7-vue';
-
-  export default {
-    components: {
-      f7Page,
-      f7Navbar,
-      f7BlockTitle,
-      f7Block,
-    },
-  };
+export default {
+  components: {}
+};
 </script>
--- template_src/src/assets/vue/pages/vuex.vue
+++ template_src/src/assets/vue/pages/vuex.vue
@@ -3,42 +3,33 @@
     <f7-navbar title="About F7" back-link="Back" sliding></f7-navbar>
     <f7-block-title>{{user.name ? "You are already logged" : "Please login"}}</f7-block-title>
     <f7-list>
-			<f7-list-group v-if="user.name">
-				<f7-list-item>ID: {{user.id}}</f7-list-item>
-			  <f7-list-item>Username: {{user.name}}</f7-list-item>
-			</f7-list-group>
-		</f7-list>	
+      <f7-list-group v-if="user.name">
+        <f7-list-item>ID: {{user.id}}</f7-list-item>
+        <f7-list-item>Username: {{user.name}}</f7-list-item>
+      </f7-list-group>
+    </f7-list>
     <f7-list>
-		  <f7-list-button color="blue" v-on:click="loginAction">Login Button</f7-list-button>
-		</f7-list>
+      <f7-list-button color="blue" v-on:click="loginAction">Login Button</f7-list-button>
+    </f7-list>
   </f7-page>
 </template>
 <script>
-	import { mapState, mapActions } from 'vuex';
-	import { f7Page, f7Navbar, f7BlockTitle, f7List, f7ListGroup, f7ListItem, f7ListButton } from "framework7-vue";
+import { mapState, mapActions } from "vuex";
 
-  export default {
-		components: {
-			f7Page, 
-			f7Navbar, 
-			f7BlockTitle, 
-			f7List, 
-			f7ListGroup, 
-			f7ListItem, 
-			f7ListButton
-		},
-  	computed: {
-        ...mapState({
-            user: state => state.user
-        })
+export default {
+  components: {},
+  computed: {
+    ...mapState({
+      user: state => state.user
+    })
+  },
+  methods: {
+    loginAction: function() {
+      const self = this;
+      // Set new user into storage
+      self.userLogged({ name: "User", id: "#1" });
     },
-		methods: {
-			loginAction: function() {
-				const self = this;
-				// Set new user into storage
-        self.userLogged({name: "User", id: "#1"});
-			},
-			...mapActions(['userLogged'])
-		}
-  };
+    ...mapActions(["userLogged"])
+  }
+};
 </script>
